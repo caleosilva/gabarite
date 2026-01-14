a718cfbe91aa74efb5e7437ca68d9f6091e7006f
Caleo Silva <caleo.pinheiro@softwaredata.com.br>
2026-01-14 14:01:54 -0300

    feat: action cadastro usuario

---
+++ app/api/usuario/actions/ActionCadastrarUsuario.ts
@@ -0,0 +1,43 @@
+import { Model } from "mongoose";
+import { BaseActionCadastrar } from "@/commons/servidor/BaseActionCadastrar/BaseActionCadastrar";
+import { AppError, HttpCode } from "@/commons/servidor/AppError/AppError";
+import { UsuarioType } from "@/models/Usuario";
+import {CryptoUtils} from "@/utils/CryptoUtils"
+
+export class CadastrarUsuarioAction extends BaseActionCadastrar<UsuarioType> {
+  constructor(model: Model<UsuarioType>) {
+    super(model);
+  }
+
+  protected async validate(data: Partial<UsuarioType>): Promise<void> {
+    // 1. Executa a validação básica da classe pai
+    await super.validate(data);
+
+    if (!data.email) {
+      throw new AppError(HttpCode.BAD_REQUEST, "O e-mail é obrigatório.");
+    }
+
+    // 2. Busca se já existe um usuário (não excluído) com este e-mail
+    const usuarioExistente = await this.model.findOne({ 
+      email: data.email.toLowerCase(),
+      excluido: false 
+    }).lean();
+
+    if (usuarioExistente) {
+      throw new AppError(
+        HttpCode.CONFLICT,
+        "Este e-mail já está cadastrado no sistema."
+      );
+    }
+  }
+
+  protected async save(data: Partial<UsuarioType>): Promise<UsuarioType> {
+    const payload = { ...data };
+
+    if (payload.senha) {
+      payload.senha = await CryptoUtils.hashPassword(payload.senha);
+    }
+
+    return await super.save(payload);
+  }
+}
